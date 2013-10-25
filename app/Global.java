860888c1a10cb21ca9f063558ab8d0caeda7ce38
Eduard Gamiao <edgamiao@gmail.com>
2013-10-24 19:29:33 -1000

    ShowSurfer change.
    Edited ShowSurfer to include inline CSS styles.

--- app/Global.java
+++ app/Global.java
@@ -18,7 +18,7 @@ public class Global extends GlobalSettings {
    */
   public void onStart(Application app) {
     SurferDB.addSurfer(new SurferFormData("Eduard", "Hawaii", null, "URL", "URL", "Lol", "eduard", "Male"));
-    SurferDB.addSurfer(new SurferFormData("Jayson", "Germany", null, "URL", "URL", "Lol", "jayson", "Female"));
-    SurferDB.addSurfer(new SurferFormData("Lenie", "China", null, "URL", "URL", "Lol", "eduard", "Male"));    
+    SurferDB.addSurfer(new SurferFormData("Jayson", "Germany", null, "http://cdn1.cdnme.se/cdn/7/2677323/images/2012/trololo-face_195014788.jpg", "URL", "Lol", "jayson", "Female"));
+    SurferDB.addSurfer(new SurferFormData("Lenie", "China", null, "http://www.surfline.com/surfaz/images/ho_michael/1chang_mho_winner.jpg", "URL", "Lol", "eduard", "Male"));    
   }
 }
